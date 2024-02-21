8483a7072a683fc83ab579d1e7a8b3de066780b3
Tu Nguyen Ngoc <nguyenngoctu1205@gmail.com>
2024-02-22 00:10:35 +0700

    Update home page

---
+++ static/js/base/base_js.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Onscroll fixed top navigation
+  window.addEventListener('scroll', function() {
+    const navigation = document.querySelector('.navigation');
+    if (window.scrollY >= 60) {
+      navigation.classList.add('sticky-md-top', 'sticky-navigation', 'shadow');
+    } else {
+      navigation.classList.remove('sticky-md-top', 'sticky-navigation', 'shadow');
+    }
+  });
+
+  // Dark theme toggle
+  const darkThemeBtn = document.querySelector('.darkthemebtn');
+  darkThemeBtn.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+  });
+
+  // Back to top button
+  const backToTopBtn = document.querySelector('.back-to-top');
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= 60) {
+      backToTopBtn.classList.add('d-block');
+    } else {
+      backToTopBtn.classList.remove('d-block');
+    }
+  });
+  backToTopBtn.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
