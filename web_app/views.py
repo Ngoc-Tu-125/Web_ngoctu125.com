4d1ee25d493b0be6b8532fcb2be58a1eeefa42fa
Tu Nguyen Ngoc <nguyenngoctu1205@gmail.com>
2024-02-23 13:58:37 +0700

    Update the tech sharing page

--- web_app/views.py
+++ web_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def tech_sharing(request):
+    return render(request, 'tech_sharing.html')
--- web_manage/urls.py
+++ web_manage/urls.py
@@ -21,4 +21,5 @@ from web_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('tech-sharing/', views.tech_sharing, name='tech_sharing'),
 ]
