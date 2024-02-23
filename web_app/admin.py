433e5c40a365c1fa1bbe6a1857283aed72d87fbb
Tu Nguyen Ngoc <nguyenngoctu1205@gmail.com>
2024-02-23 18:47:39 +0700

    Update the admin

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TechSharing, Tag
 
-# Register your models here.
+class TechSharingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_published', 'slug')
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Tag)
+admin.site.register(TechSharing, TechSharingAdmin)
