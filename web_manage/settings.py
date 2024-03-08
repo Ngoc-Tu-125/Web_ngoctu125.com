405ba9fec36729c5b5cf3fa28bd6df5fc088c803
Tu Nguyen Ngoc <nguyenngoctu1205@gmail.com>
2024-03-09 03:02:19 +0700

    fix path db

--- web_manage/settings.py
+++ web_manage/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'web_manage.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('DB_NGOCTU125_WEB', 'ngoctu125_web'),
+        'NAME': '/var/www/ngoctu125_web/ngoctu125_web.db',
         'USER': 'ngoctu125',
         'PASSWORD': '120598',
         'HOST': 'localhost',
